26cda7a52ac5a3521f6fa0a2022023f2002a58dd
Pasindu Nuwantha <pasindunuwantha19@gmail.com>
2025-09-02 02:42:10 +0530

    Can open Menu and Close Menu From Mobile Screen

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react'
 import { assets, menuLinks } from '../assets/assets'
-import {Link, useLocation} from 'react-router-dom'
+import {Link, useLocation, useNavigate} from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
     const location = useLocation()
 
     const [open, setOpen] = useState(false)
+    const navigate = useNavigate()
 
   return (
     <div className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 
       border-b border-borderColor relative transition-all ${location.pathname === "/" && "bg-light"}`}>
 
        <Link to='/'>
-       <img src={assets.logo} alt="logo" style={{ height: '70px', width: 'auto'}} className="h-20 w-auto"/> 
+       <img src={assets.logo} alt="logo" style={{ height: '60px', width: 'auto', transform: 'translate(-20px, -10px)'}} className="h-8 flex-shrink-0"/> 
        </Link>
 
        <div className= {`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${location.pathname === "/" ? "bg-light" : "bg-white"} ${open ? "max-sm:translate-x-0" : "max-sm:translate-x-full"} `} >
@@ -30,13 +31,18 @@ const Navbar = () => {
             <input type="text" className="py-1.5 w-full bg-trnsparent outline-none placeholder-grey-500" placeholder="Search Products"></input>
             <img src={assets.search_icon} alt="search" />
         </div>
-        <div>
-            <button className="cursor-pointer">Dashboard</button>
-            <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg" >Login</button>
+        
+        <div className="flex max-sm : flex-row items-start sm:items-center gap-6">
+            
+            <button onClick={()=>navigate ('/owner')} className="cursor-pointer">Dashboard</button>
+            <button onClick={()=> setShowLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg ">Login</button>
+
         </div>
-
-
        </div>
+
+       <button className='sm:hidden cursor-pointer' aria-lable= "Menu" onClick={()=> setOpen(!open)}>
+        <img src={open ? assets.close_icon : assets.menu_icon} alt="menu" />
+       </button>
         
     </div>
   )
